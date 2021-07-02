25f06a0c9c22bb584e4b8347b24b30c23b61e296
Lotfi Anwar L Arif <52082662+Lotfi-Arif@users.noreply.github.com>
2021-07-02 15:28:00 +0800

    Fix DB pass

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { Banner } from './banners/entities/banner.entity';
       host: 'db',
       port: 3306,
       username: 'root',
-      password: 'password',
+      password: 'Web.Tech123',
       database: 'dev_profile',
       autoLoadEntities: true,
       entities: [Banner],
